fabc62983d269d2d1a5912312c609e66008c8477
Sunil Kumar <147473716+skmachra@users.noreply.github.com>
2023-10-21 13:16:48 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -654,12 +654,13 @@ async def send_all(bot, userid, files, ident):
                 protect_content=True if ident == "filep" else False,
                 reply_markup=InlineKeyboardMarkup(
                     [
-                        [
-                        InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=GRP_LNK),
+                    [
+                        InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=GRP_LNK)
+                    ],[
                         InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
                     ],[
-                        InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/creatorbeatz")
-                        ]
+                        InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/skcontactbot")
+                      ]
                     ]
                 )
             )
