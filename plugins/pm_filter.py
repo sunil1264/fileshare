b14016658079aaab984e7072ac42079e276b0448
Sunil Kumar <147473716+skmachra@users.noreply.github.com>
2024-01-07 21:55:23 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -510,38 +510,38 @@ async def select_language(bot, query):
 
 @Client.on_callback_query(filters.regex(r"^spol"))
 async def advantage_spoll_choker(bot, query):
-    if query.message.chat.type == "private":
-        _, user, movie_ = query.data.split('#')
-        movies = SPELL_CHECK.get(query.message.reply_to_message.id)
-        if not movies:
-            return await query.answer(script.OLD_ALRT_TXT.format(query.from_user.first_name), show_alert=True)
-            if int(user) != 0 and query.from_user.id != int(user):
-                return await query.answer(script.ALRT_TXT.format(query.from_user.first_name), show_alert=True)
-                if movie_ == "close_spellcheck":
-                    return await query.message.delete()
-                    movie = movies[(int(movie_))]
-                    await query.answer(script.TOP_ALRT_MSG)
-                    gl = await global_filters(bot, query.message, text=movie)
-                    if gl == False:
-                        k = await manual_filters(bot, query.message, text=movie)
-                        if k == False:
-                            files, offset, total_results = await get_search_results(query.message.chat.id, movie, offset=0, filter=True)
-                            if files:
-                                k = (movie, files, offset, total_results)
-                                await auto_filter(bot, query, k)
-                            else:
-                                reqstr1 = query.from_user.id if query.from_user else 0
-                                reqstr = await bot.get_users(reqstr1)
-                                if NO_RESULTS_MSG:
-                                    await bot.send_message(chat_id=LOG_CHANNEL, text=(script.NORSLTS.format(reqstr.id, reqstr.mention, movie)))
-                                    if query.message.chat.type == "private":
-                                        response_text = script.MVE_NT_FND
-                                        await bot.send_message(chat_id=query.from_user.id, text=response_text)
-                                    else:
-                                        k = await query.message.reply_text(script.MVE_NT_FND)
-                                        await asyncio.sleep(10)
-                                        await k.delete()
-        else:
+    # if query.message.chat.type == "private":
+    _, user, movie_ = query.data.split('#')
+    movies = SPELL_CHECK.get(query.message.reply_to_message.id)
+    if not movies:
+        return await query.answer(script.OLD_ALRT_TXT.format(query.from_user.first_name), show_alert=True)
+        if int(user) != 0 and query.from_user.id != int(user):
+            return await query.answer(script.ALRT_TXT.format(query.from_user.first_name), show_alert=True)
+            if movie_ == "close_spellcheck":
+                return await query.message.delete()
+                movie = movies[(int(movie_))]
+                await query.answer(script.TOP_ALRT_MSG)
+                gl = await global_filters(bot, query.message, text=movie)
+                if gl == False:
+                    k = await manual_filters(bot, query.message, text=movie)
+                    if k == False:
+                        files, offset, total_results = await get_search_results(query.message.chat.id, movie, offset=0, filter=True)
+                        if files:
+                            k = (movie, files, offset, total_results)
+                            await auto_filter(bot, query, k)
+                        else:
+                            reqstr1 = query.from_user.id if query.from_user else 0
+                            reqstr = await bot.get_users(reqstr1)
+                            if NO_RESULTS_MSG:
+                                await bot.send_message(chat_id=LOG_CHANNEL, text=(script.NORSLTS.format(reqstr.id, reqstr.mention, movie)))
+                                if query.message.chat.type == "private":
+                                    response_text = script.MVE_NT_FND
+                                    await bot.send_message(chat_id=query.from_user.id, text=response_text)
+                                else:
+                                    k = await query.message.reply_text(script.MVE_NT_FND)
+                                    await asyncio.sleep(10)
+                                    await k.delete()
+  '''      else:
             _, user, movie_ = query.data.split('#')
             movies = SPELL_CHECK.get(query.message.reply_to_message.id)
             if not movies:
@@ -571,7 +571,7 @@ async def advantage_spoll_choker(bot, query):
                                         else:
                                             k = await query.message.reply_text(script.MVE_NT_FND)
                                             await asyncio.sleep(10)
-                                            await k.delete()
+                                            await k.delete()'''
                     
 
 
